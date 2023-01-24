65c21b9a99d3b4f28f1bb8c2176b842e304128bc
scriblical <scriblical@gmail.com>
2023-01-23 23:53:36 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState} from "react";
+
 
 function App() {
+    const [name, setName] = useState({firstName: 'Chuck'});
+    const [age, setAge] = useState(0);
+
+    const handleClick    = () => {
+        setName({firstName: 'Chuck'});
+        setAge(age + 1);
+
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>My name is {name['firstName']}</h1>
+        <h1>I like to count. {age + 1}</h1>
+        <button onClick={handleClick}>Click me</button>
     </div>
+
   );
 }
 
