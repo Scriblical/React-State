0f9be0b31ec99ea6ee0e386d4da0543c20516acf
scriblical <scriblical@gmail.com>
2023-01-23 23:58:35 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,20 +3,20 @@ import { useState} from "react";
 
 
 function App() {
-    const [name, setName] = useState({firstName: 'Chuck'});
+
     const [age, setAge] = useState(0);
 
     const handleClick    = () => {
-        setName({firstName: 'Chuck'});
+
         setAge(age + 1);
 
     }
 
   return (
     <div className="App">
-        <h1>My name is {name['firstName']}</h1>
-        <h1>I like to count. {age + 1}</h1>
-        <button onClick={handleClick}>Click me</button>
+
+        <h1>I like to count: {age + 1}</h1>
+        <button onClick={handleClick}>Click me to + 1</button>
     </div>
 
   );
